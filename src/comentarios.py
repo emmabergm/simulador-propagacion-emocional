7cbfd317ee332f15ad69376aff58d7be88fb98fa
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-04 14:13:29 -0300

    Create comentarios.py

---
+++ src/comentarios.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 14:01:36 2026
+
+@author: olivi
+"""
+
+def guardar_comentario(estudiante_1, situacion, archivo): 
+    '''
+    Guardar la eleccion de comentario del estudiante en cada situacion
+
+    Parameters
+    ----------
+    estudiante_1 : str
+        estudiante que elige un comentario en base a un contexto 
+    
+    situacion: str
+        Parciales o no parciales 
+    
+    archivo: cvs
+        Archivo en donde se encuntran las opciones de comentarios y se guarda la elegida
+        
+
+    Returns
+    -------
+    None.
+
+    '''
+    pass 
