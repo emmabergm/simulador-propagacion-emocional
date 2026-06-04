3752f7aecc8f51da2e513c0a44e81fd1e990e073
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-04 14:19:38 -0300

    Update comentarios.py

--- src/comentarios.py
+++ src/comentarios.py
@@ -4,18 +4,30 @@ Created on Thu Jun  4 14:01:36 2026
 
 @author: olivi
 """
+def realizar_pregunta(estudiante_1): 
+    '''
+    Funcion que pregunta en que contexto se encuentra (parciales/no parciales) y genera la pregunta
+    
+    Parameters
+    ----------
+    estudiante_1 : str
+        nombre del estudiante que responde la pregunta 
 
-def guardar_comentario(estudiante_1, situacion, archivo): 
+    Returns
+    -------
+    situacion, respuesta 
+
+    '''
+    pass
+
+def guardar_comentario(funcion_pregunta, archivo): 
     '''
     Guardar la eleccion de comentario del estudiante en cada situacion
 
     Parameters
     ----------
-    estudiante_1 : str
-        estudiante que elige un comentario en base a un contexto 
-    
-    situacion: str
-        Parciales o no parciales 
+    funcion_pregunta: str
+        La situacion y respuesta que ingreso el estudiante 
     
     archivo: cvs
         Archivo en donde se encuntran las opciones de comentarios y se guarda la elegida
@@ -23,7 +35,7 @@ def guardar_comentario(estudiante_1, situacion, archivo):
 
     Returns
     -------
-    None.
+    archivo
 
     '''
     pass 
