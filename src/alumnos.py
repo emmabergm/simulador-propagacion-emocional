9c4f6561330f2159e51163e1e52c9361152d52f3
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-04 14:27:29 -0300

    Update alumnos.py

--- src/alumnos.py
+++ src/alumnos.py
@@ -1,1 +1,52 @@
 
+
+
+def info_estudiante2 (estudiante_2):
+    '''
+    Le pregunta al estudiante por su informacion actual neutra 
+
+    Parameters
+    ----------
+    estudiante_2 : str
+        Nombre del estudiante dos que va a valorar sus emociones a partir de los comentarios 
+
+    Returns
+    -------
+    edad, valoracion neutra para cada emocion
+
+    '''
+    pass
+
+def guardar_archivo_neutro(funcion_info): 
+    '''
+    Agregar la informacion del estudiante_2 al archivo de emociones neutras 
+
+    Parameters
+    ----------
+    funcion_info : int 
+        La funcion va a retornar la edad en int y la valoracion por emocion en int tambien 
+
+    Returns
+    -------
+    None.
+
+    '''
+    pass 
+
+def valoracion_comentario(archivo_comentario, archivo_emociones): 
+    '''
+    Presentarle al estudiante_2 el comentario elegido por el estudiante_1 y preguntarle su valoracion
+    (Van a presentarse 2 comentarios)
+    Parameters
+    ----------
+    archivo_comentario : cvs
+        archivo que tiene guardado el comentario que eligio el estudiante_1
+    archivo_emociones: cvs
+        archivo en el que se va a guardar la valoracion que haga el estudiante_2 del coemntario 
+
+    Returns
+    -------
+    None.
+
+    '''
+    pass
