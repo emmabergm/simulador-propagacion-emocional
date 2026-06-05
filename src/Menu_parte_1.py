731463be0bedbc9c9b0d4bcb4512d3f5a0e36438
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-05 12:04:27 -0300

    Create Menu_parte_1.py

---
+++ src/Menu_parte_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 12:02:31 2026
+
+@author: olivi
+"""
+
+def menu_parte_1(): 
+    '''
+    Pregunta si desea salir o seguir con la Parte 2, si desea salir 
+    se imprime un mensaje agradeciendo la participacion y se corta el programa
+
+    Returns
+    -------
+    None.
+
+    '''
