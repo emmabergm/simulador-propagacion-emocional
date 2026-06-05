9b2d79273403937e26c7c2ee57b70fd068efcdd7
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-05 12:21:45 -0300

    Update devolucion.py

--- src/devolucion.py
+++ src/devolucion.py
@@ -1,15 +1,2 @@
-def menu_2(): 
-  '''
-  Le da opciones al usuario de que devolucion quiere ver: 
-  - Feedback de como influyen los comentarios 
-  - Grafico neutro y grafico post que compare el cambio 
-  - promedio grupal de emcoion a partir de cada comentario 
 
-  Returns: 
-  ----------
-  str: opcion elegida 
-  '''
-  pass 
-
-def feedback_emociones(menu_2, archivo_e_n, archivo_e_post): 
   
