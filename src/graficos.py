1dec6a05aaf50c02987f2b3a8d20f54e94ef01d3
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-08 18:02:14 -0300

    Update graficos.py

--- src/graficos.py
+++ src/graficos.py
@@ -1,1 +1,26 @@
 
+
+
+import matplotlib.pyplot as plt
+
+def grafico(archivo_comentarios, archivo_e_n, archivo_e_comentario1, archivo_e_comentario2): 
+    '''
+    Funcion que crea el grafico a partir de los datos
+
+    Parameters
+    ----------
+    archivo_comentarios : csv
+        Archivo que contiene los dos comentarios elegidos por el usuario 
+    archivo_e_n : csv
+        Archivo que contiene datos de las emociones neutras de los estudiantes 
+    archivo_e_comentario1 : csv
+        Archivo que contiene datos de las emcoiones despues que se haga el coemntario 1
+    archivo_e_comentario2 : csv
+        Archivo que contiene datos de las emcoiones despues que se haga el coemntario 2
+
+    Returns
+    -------
+    Grafico 
+
+    '''
+    pass 
