0b1c5af7f7310ac7b1d0b0e59964ae230c443c4e
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-08 17:45:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
+from src.comentarios import situacion
+from src.comentarios import realizar_pregunta
+from src.comentarios import guardar_respuesta
+from src.alumnos import 
+from src.analisis import 
+from src.validacion_datos import 
+from src.graficos import 
+from src.devolucion import 
+
+
 estudiante_1 = input("Ingrese su nombre: ")
 menu_1 = "funcion de menu"
