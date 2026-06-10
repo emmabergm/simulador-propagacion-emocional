6b858c1417ebfe4c36debc5f00b36f704af80f13
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-10 16:00:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 from src.comentarios import situacion
 from src.comentarios import realizar_pregunta
+from src.comentarios import asociacion
 from src.alumnos import info_estudiante2 
 from src.alumnos import valoracion_comentario
-from src.analisis import  
-from src.validacion_datos import 
-from src.graficos import 
-from src.devolucion import 
+from src.analisis import  unir_datos_emocionales
+from src.analisis import calcualar_promedios_grupales
+from src.analisis import comparar_promedios
+from src.analisis import feedback_comentario
 
+#from src.validacion_datos import 
+#from src.graficos import 
+#from src.devolucion import 
 
+# Abrir archivos: 
 archivo_neutro= "C:/Users/camil/OneDrive/Desktop/Simulador emocional/simulador-propagacion-emocional/archivos/archivo_e_n.xlsx"
 
 archivo_e_comentario1= "C:/Users/camil/OneDrive/Desktop/Simulador emocional/simulador-propagacion-emocional/archivos/archivo_e_post.xlsx"
@@ -26,13 +31,27 @@ try:
     df_tranquilidad=pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_tranquilidad")
     df_estres=pd.read_excel(archivo_e_comentario1, sheet_name= "Comentario_estres")
  
- except FileNotFoundError: 
-         print("El archivo no se encontro")
+except FileNotFoundError: 
+    print("El archivo no se encontro")
 
+
+#Parte 1: 
 
 estudiante_1 = input("Ingrese su nombre: ")
+
+situacion_estudiante = situacion()
+planteo_situacion_1 = realizar_pregunta(df_comentario)
+primer_comentario = funcion(situacion_estudiante, planteo_situacion_1)
+
+situacion_estudiante = situacion()
+planteo_situacion_2 = realizar_pregunta(df_comentario)
+
+respuesta_1 = realizar_pregunta(df_comentario) 
+
+
 if menu_1 == "True": 
   estudiante_2 = input("Ingrese su nombre: ")
+  
 else: 
   print("Muchas gracias por su respuesta!") #NO ESTA BIEN 
 edad_e_2 = int(input("Ingrese su edad: ")
