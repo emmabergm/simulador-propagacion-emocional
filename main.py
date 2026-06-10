796046d0789256657adbbded81f687e4bfe58ec6
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-10 09:50:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import pandas as pd
 from src.comentarios import situacion
 from src.comentarios import realizar_pregunta
 from src.comentarios import guardar_respuesta
-from src.alumnos import 
-from src.analisis import 
+from src.alumnos import info_estudiante2 
+from src.analisis import cargar_datos_emocionales
 from src.validacion_datos import 
 from src.graficos import 
 from src.devolucion import 
+
+
+archivo_neutro= "C:/Users/camil/OneDrive/Desktop/Simulador emocional/simulador-propagacion-emocional/archivos/archivo_e_n.xlsx"
+
+archivo_e_comentario1= "C:/Users/camil/OneDrive/Desktop/Simulador emocional/simulador-propagacion-emocional/archivos/archivo_e_post.xlsx"
 
 
 estudiante_1 = input("Ingrese su nombre: ")
