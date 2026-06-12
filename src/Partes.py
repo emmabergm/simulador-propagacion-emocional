a3f333f4cc6230e6dc3013345cb211c5bdeeb9af
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-12 12:52:24 -0300

    Create Partes.py

---
+++ src/Partes.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 12:18:42 2026
+
+@author: olivi
+"""
+
+def parte_1(situacion, menu_principal respuesta, guardar_rta, asociacion): 
+    
+        rta = respuesta 
+        guardar_respuesta = guardar_rta
+        df_respuesta = guardar_respuesta(respuesta_1, indice, df_comentario, situacion_1 )
+        df_asociado_1 = asociado(respuesta_1, df_tranquilidad, df_motivacion, df_estres)
+        
+        return df_respuesta 
+    
+        
+        
+def parte_2()
+    
+    
