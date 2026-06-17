026033da1c1dd645337f2d4a11d31bd2649371ff
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-17 11:25:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ from src.comentario import situacion
 from src.comentario import numero_random
 from src.comentario import asociacion
 from src.Menus import menu_inicio
+from src.Menus import menu_situacion
+from src.Menus import menu_com2
 from src.Menus import menu_parte2 
 from src.Menus import menu_parte_3
 from src.Partes import parte_1
 from src.Partes import parte_2
+from src.Partes import parte_2_comentario2
 
 
 #from src.validacion_datos import 
@@ -58,18 +61,33 @@ try:
         indice = numero_random(df_comentario)
         parte_1_sit1 = parte_1(tipo_situacion, indice, df_comentario)
         df_asociado_1 = asociacion(parte_1_sit1, df_tranquilidad, df_motivacion, df_estres)
-        tipo_situacion_2 = situacion()
+        menu_sit = menu_situacion(tipo_situacion)
+        if menu_sit == "continuar": 
+        
+            while True:
+                tipo_situacion_2 = situacion()
+                if tipo_situacion_2 == tipo_situacion:
+                    raise ValueError("Debe ingresar una situacion diferente a la anterior")
+                else: 
+                    break 
+            
+       
         parte_1_sit2 = parte_1(tipo_situacion_2, indice, df_comentario)
         df_asociado_2 = asociacion(parte_1_sit2, df_tranquilidad, df_motivacion, df_estres)
         
 # Parte 2 
         menu_2 = menu_parte2() 
         if menu_2 == "si": 
-            comentario_1 = parte_2(df_neutro, df_comentario, parte_1_sit1, indice, tipo_situacion, df_asociado_1)
-            comentario_2 = parte_2(df_neutro, df_comentario, parte_1_sit2, indice, tipo_situacion_2, df_asociado_2)
+            comentario_1, estudiante_2, edad_2 = parte_2(df_neutro, df_comentario, parte_1_sit1, indice, tipo_situacion, df_asociado_1)
+
+            menu_com = menu_com2()
+            if menu_com == "continuar": 
+                comentario_2 = parte_2_comentario2(df_comentario, parte_1_sit2, indice, tipo_situacion_2, df_asociado_2, estudiante_2, edad_2)
             
+           
+                
 # Parte 3  
-            menu_3 = menu_parte_3(df_neutro, df_asociado_1, df_asociado_2, tipo_situacion, df_comentario, parte_1_sit1, parte_1_sit2, indice, comentario_1)
+                menu_3 = menu_parte_3(df_neutro, df_asociado_1, df_asociado_2, tipo_situacion, df_comentario, parte_1_sit1, parte_1_sit2, indice, comentario_1)
     
     elif menu_principal == "si": 
         menu_2 = menu_parte2() 
