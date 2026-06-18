b4f7aff89d65c99eb6cf6d273ae4494a9180b979
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-18 16:41:28 -0300

    Update graficos.py

--- src/graficos.py
+++ src/graficos.py
@@ -71,27 +71,3 @@ def grafico(df_neutro, df_asociado_1, df_asociado_2):
 
     plt.show()
         
-
-    
-   
-    '''
-    Funcion que crea el grafico a partir de los datos
-
-    Parameters
-    ----------
-    archivo_comentarios : csv
-        Archivo que contiene los dos comentarios elegidos por el usuario 
-    archivo_e_n : csv
-        Archivo que contiene datos de las emociones neutras de los estudiantes 
-    archivo_e_comentario1 : csv
-        Archivo que contiene datos de las emcoiones despues que se haga el coemntario 1
-    archivo_e_comentario2 : csv
-        Archivo que contiene datos de las emcoiones despues que se haga el coemntario 2
-
-    Returns
-    -------
-    Grafico 
-
-    '''
-   
-    pass 
