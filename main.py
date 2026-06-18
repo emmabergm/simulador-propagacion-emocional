575f7701c2d4ee895c97416f10361da8272cf669
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-18 16:41:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import os
 
 from src.comentario import situacion
 from src.comentario import numero_random
 from src.comentario import asociacion
-from src.Menus import menu_inicio
+from src.Menus import menu_grupos
 from src.Menus import menu_situacion
 from src.Menus import menu_com2
 from src.Menus import menu_parte2 
@@ -15,107 +15,75 @@ from src.Partes import parte_2_comentario2
 
 
 
+archivo_grupos = "grupos.csv"
+
+if os.path.exists(archivo_grupos):
+    df_grupos = pd.read_csv(archivo_grupos)
+    
+else:
+    df_grupos = pd.DataFrame(columns=["nombre", "situacion", "situacion_2"])
+    
+
+nombre_grupo = input("Ingrese el nombre de su grupo: ")
+
+tipo_situacion, tipo_situacion_2 = menu_grupos(df_grupos, nombre_grupo)
+
+if tipo_situacion is not None:
+    print("Ese grupo ya esta registrado")
+    menu_principal = "si"
+
+
 archivo_neutro = "archivos/archivo_e_n.xlsx"
 archivo_e_comentario1 = "archivos/archivo_e_post.xlsx"
 archivo_comentario = "archivos/archivo_comentario (1).xlsx"
 
-
+# Carga de archivos:
 try: 
-     
-    df_neutro= pd.read_excel(archivo_neutro)
+    df_neutro = pd.read_excel(archivo_neutro)
     df_neutro.columns = df_neutro.columns.str.strip()
 
- 
     df_comentario = pd.read_excel(archivo_comentario)
     df_comentario.columns = df_comentario.columns.str.strip()
-    
-    df_motivacion= pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_motivacional")
+
+    df_motivacion = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_motivacional")
     df_motivacion.columns = df_motivacion.columns.str.strip()
-    
-    df_tranquilidad=pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_tranquilidad")
+
+    df_tranquilidad = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_tranquilidad")
     df_tranquilidad.columns = df_tranquilidad.columns.str.strip()
 
-    df_estres=pd.read_excel(archivo_e_comentario1, sheet_name= "Comentario_estres")
+    df_estres = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_estres")
     df_estres.columns = df_estres.columns.str.strip()
-
-    
-
-
- 
 except FileNotFoundError: 
     print("El archivo no se encontro")
-    
 
+# Logica del programa:
 
-try: 
-    nombre_grupo = input("Ingrese el nombre de su grupo: ")
-    tipo_situacion = situacion()
-    menu_principal = menu_inicio(nombre_grupo, tipo_situacion)
-except ValueError as e:
-    print("Error: ", e)
+tipo_situacion = situacion()
+menu_principal = menu_inicio(nombre_grupo, tipo_situacion)
 
-    
-if menu_principal == "no": 
-        try: 
-            indice = numero_random(df_comentario)
-            parte_1_sit1 = parte_1(tipo_situacion, indice, df_comentario)
-            df_asociado_1 = asociacion(parte_1_sit1, df_tranquilidad, df_motivacion, df_estres)
-            df_asociado_1 = asociacion(parte_1_sit1, df_tranquilidad, df_motivacion, df_estres)
-            menu_sit = menu_situacion(tipo_situacion)
-        except ValueError as e:
-            print("Error: ", e)
+if menu_principal == "no":
+    indice = numero_random(df_comentario)
+    parte_1_sit1 = parte_1(tipo_situacion, indice, df_comentario)
+    df_asociado_1 = asociacion(parte_1_sit1, df_tranquilidad, df_motivacion, df_estres)
 
-        if menu_sit == "continuar": 
-        
-            while True:
-                try: 
-                    tipo_situacion_2 = situacion()
-                    if tipo_situacion_2 == tipo_situacion:
-                        raise ValueError("Debe ingresar una situacion diferente a la anterior")
-                    else: 
-                        break 
-                    
-                except ValueError as e: 
-                    print(e)
-                    continue 
-            
-       
+    menu_sit = menu_situacion(tipo_situacion)
+    if menu_sit == "continuar":
+        while True:
+            tipo_situacion_2 = situacion()
+            if tipo_situacion_2 == tipo_situacion:
+                print("Debe ingresar una situacion diferente a la anterior")
+                continue
+            break
+
         parte_1_sit2 = parte_1(tipo_situacion_2, indice, df_comentario)
         df_asociado_2 = asociacion(parte_1_sit2, df_tranquilidad, df_motivacion, df_estres)
 
-        menu_2 = menu_parte2() 
-        if menu_2 == "si": 
+        menu_2 = menu_parte2()
+        if menu_2 == "si":
             comentario_1, estudiante_2, edad_2 = parte_2(df_neutro, df_comentario, parte_1_sit1, indice, tipo_situacion, df_asociado_1)
 
             menu_com = menu_com2()
-            if menu_com == "continuar": 
+            if menu_com == "continuar":
                 comentario_2 = parte_2_comentario2(df_comentario, parte_1_sit2, indice, tipo_situacion_2, df_asociado_2, estudiante_2, edad_2)
-            
-           
-                
-  
-                menu_3 = menu_parte_3(df_neutro, df_asociado_1, df_asociado_2, tipo_situacion,tipo_situacion_2, df_comentario, parte_1_sit1, parte_1_sit2, indice, comentario_1)
-    
-    elif menu_principal == "si": 
-        menu_2 = menu_parte2() 
-        if menu_2 == "si": 
-            comentario_1 = parte_2(df_neutro, df_comentario, parte_1_sit1, indice, tipo_situacion, df_asociado_1)
-            comentario_2 = parte_2(df_neutro, df_comentario, parte_1_sit2, indice, tipo_situacion_2, df_asociado_2)
-            menu_3 = menu_parte_3(df_neutro, df_asociado_1, df_asociado_2, tipo_situacion, df_comentario, parte_1_sit1, parte_1_sit2, indice, comentario_1)
-
-except ValueError as e:
-    print("Error: ", e)
-    
-
-
-    
-
-
-
-
-
-
-
-
-
+                menu_parte_3(df_neutro, df_asociado_1, df_asociado_2, tipo_situacion, tipo_situacion_2, df_comentario, parte_1_sit1, parte_1_sit2, indice, comentario_1)
 
