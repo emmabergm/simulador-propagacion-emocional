65eb0bb6b687ca8d78377c47cd704a08c510ba52
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-06-19 12:43:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,26 @@ columnas = ["nombre", "situacion", "situacion_2", "indice", "respuesta_1", "resp
 try:
     df_neutro = pd.read_excel(archivo_neutro)
     df_neutro.columns = df_neutro.columns.str.strip()
+    df_neutro[["estres", "motivacion", "tranquilidad"]] = df_neutro[["estres", "motivacion", "tranquilidad"]] * 100   
+
     df_comentario = pd.read_excel(archivo_comentario)
     df_comentario.columns = df_comentario.columns.str.strip()
+    
     df_comentario["parciales"] = df_comentario["parciales"].astype(str)
     df_comentario["no_parciales"] = df_comentario["no_parciales"].astype(str)
+    
     df_motivacion = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_motivacional")
     df_motivacion.columns = df_motivacion.columns.str.strip()
+    df_motivacion[["estres", "motivacion", "tranquilidad"]] = df_motivacion[["estres", "motivacion", "tranquilidad"]] * 100 
+   
     df_tranquilidad = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_tranquilidad")
     df_tranquilidad.columns = df_tranquilidad.columns.str.strip()
+    df_tranquilidad[["estres", "motivacion", "tranquilidad"]] = df_tranquilidad[["estres", "motivacion", "tranquilidad"]] * 100 
+    
     df_estres = pd.read_excel(archivo_e_comentario1, sheet_name="Comentario_estres")
     df_estres.columns = df_estres.columns.str.strip()
+    df_estres[["estres", "motivacion", "tranquilidad"]] = df_estres[["estres", "motivacion", "tranquilidad"]] * 100 
+    
 except FileNotFoundError:
     print("El archivo no se encontro")
 
